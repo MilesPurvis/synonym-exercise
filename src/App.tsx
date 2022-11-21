97b1d2a19d96969874493a48f6a3f3d899c4bf24
Miles Purvis <milesjsp@gmail.com>
2022-11-20 21:16:17 -0500

    commit synonym app

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./App.css";
+import { useGetSynonyms } from "./hooks/useGetSynonyms";
+
+function App() {
+  const [word, setWord] = useState<string>("");
+  const { isLoading, synonyms, getSynonyms } = useGetSynonyms();
+
+  const handleFetchSynonyms = (e: React.FormEvent) => {
+    e.preventDefault();
+    getSynonyms(word);
+  };
+
+  const handleSynonymClicked = (newWord: string) => {
+    setWord(newWord);
+    getSynonyms(newWord);
+  };
+
+  return (
+    <div className="App">
+      <h1>Synonym Api</h1>
+      <form onSubmit={handleFetchSynonyms}>
+        <label htmlFor="word-input">Your Word: </label>
+        <input
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+          id="word-input"
+        ></input>
+        <button>Submit</button>
+      </form>
+
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {synonyms.map((synonym) => (
+            <li
+              onClick={() => handleSynonymClicked(synonym.word)}
+              key={synonym.word}
+            >
+              {synonym.word}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default App;
